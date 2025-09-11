2e68d248c4ac44aa84b3df4c1b5dfdb6e4d2311b
TR1432 <ppfaddy@gmail.com>
2025-09-11 20:54:17 +0100

    Feat: Google console verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="_ziANvXR0i7HJkWUfVLgyILoNdIPoJhpjNfa-GCbRXA"
+        />
+      </head>
       <body className={`${sora.variable} antialiased`}>{children}</body>
     </html>
   );
